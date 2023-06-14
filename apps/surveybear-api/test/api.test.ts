8a1e77a9b62d848fc3a8618ad19bb27f3d194930
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-06-14 14:22:27 +0200

    fix test

--- apps/surveybear-api/test/api.test.ts
+++ apps/surveybear-api/test/api.test.ts
@@ -7,5 +7,5 @@ describe('Generic endpoints', () => {
   test('GET /api/todos', async () => {
     const res = await requestWithSupertest.get('/api/todos');
     expect(res.status).toEqual(200);
-  });
+  }); 
 });
