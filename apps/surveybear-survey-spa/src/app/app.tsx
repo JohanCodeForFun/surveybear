b455584963f5a691df6950fa5e0b9c1d7e6dde73
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-06-19 21:24:47 +0200

    add StarRating component for SPA homepage

--- apps/surveybear-survey-spa/src/app/app.tsx
+++ apps/surveybear-survey-spa/src/app/app.tsx
@@ -3,13 +3,27 @@ import styled from 'styled-components';
 import SurveyPage from './pages/SurveyPage';
 
 import { Route, Routes } from 'react-router-dom';
-
+import { render } from 'react-dom'
+import ReactStars from 'react-stars'
 
 
 const StyledApp = styled.div`
     // Your style here
 `;
 
+function StarRating() {
+  const ratingChanged = (newRating: number) => {
+    console.log(newRating)
+  }
+   
+  return (
+    <ReactStars
+    count={5}
+    onChange={ratingChanged}
+    size={24}
+    color2={'#ffd700'} />
+  );
+}
 
 export function App() {
 
@@ -22,7 +36,12 @@ export function App() {
         />
         <Route
           path="/"
-          element={<div>Du är på förstasidan</div>} 
+          element={
+          <>
+            <h1>Du är på första sidan!</h1>
+            <p>Vad tycker om våra tjänster på SurveyBear?</p>
+            <StarRating/>
+          </>} 
         />
       </Routes>
     </StyledApp>
