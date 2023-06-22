5ca0dea6e94ed7184c182e25415cf7dde260f535
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-06-22 11:25:16 +0200

    add default export

--- apps/surveybear-survey-spa/src/api/getSurveyById.ts
+++ apps/surveybear-survey-spa/src/api/getSurveyById.ts
@@ -1,5 +1,7 @@
 import { ISurvey } from '@surveybear/surveybear-lib';
 
-export const getSurveyById = async ( surveyId: string): Promise<ISurvey> => {
+const getSurveyById = async ( surveyId: string): Promise<ISurvey> => {
     return (await fetch(`${process.env?.['NX_REQ_URL']}/api/survey/${surveyId}`)).json();
 }
+
+export default getSurveyById;
